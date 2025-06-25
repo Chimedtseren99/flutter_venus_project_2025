192d64e25d2b997f116415765786a2b4a1692714
Venus <venus@admin-5s-iMac.local>
2025-06-25 13:14:15 +0800

    add:projecr_1

--- project_1/lib/Smoothie_blueberry_strawberry.dart
+++ project_1/lib/Smoothie_blueberry_strawberry.dart
@@ -37,7 +37,6 @@ class _SmoothieBlueberryStrawberryState
             decoration: BoxDecoration(
               color: Color(0xfffff3f3),
               borderRadius: BorderRadius.circular(30),
-
             ),
             width: double.infinity,
             child: Column(
@@ -46,8 +45,10 @@ class _SmoothieBlueberryStrawberryState
                   children: [
                     IconButton(
                       onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => DonutScreen()));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DonutScreen()));
                       },
                       icon: Icon(
                         Icons.keyboard_arrow_left_outlined,
@@ -55,15 +56,91 @@ class _SmoothieBlueberryStrawberryState
                         size: 36,
                       ),
                     ),
-                    Text(
-                        "Blueberry Strawberry",
-                      style: TextStyle(
-                      fontSize: 20,
-                        fontWeight: FontWeight.w800,
-                        fontFamily: 'Nunito',
-
-                    )),
+                    Text("Blueberry Strawberry",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Nunito',
+                        )),
                   ],
+                ),
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                    'assets/images/smoothie.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: EdgeInsets.only(top: 46, left: 20, right: 20, bottom: 31),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ingredients'),
+                        Row(
+                          children: [
+                            Container(
+                              child: Column(
+                  
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 15),
+                                    child: Text(
+                                      'Sugar',
+                                      style: TextStyle(fontWeight: FontWeight.bold,
+                                      fontSize: 15),
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Color(0xffcae1ff),
+                                    ),
+                                    width: 50,
+                                    height: 50,
+                                    child: Center(child: Text('2%',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    )),
+                  
+                                  )
+                                ],
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              ),
+                              width: 65,
+                              height: 120,
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.grey,
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            )
+                          ],
+                        ),
+
+                        Text('Details',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),),
+                        Text('A smoothie commonly has a liquid base,such as fruit juice or milk, yogurt or ice cream.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),)
+                      ],
+                    ),
+                  ),
                 )
               ],
             ),
